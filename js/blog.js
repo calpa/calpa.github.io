e437c531787a09565f415921e4e2d9ace61fbf36
Calpa Liu <calpaliu@gmail.com>
2017-06-06 23:40:21 +0800

    Site updated: 2017-06-06 23:40:20

--- js/blog.js
+++ js/blog.js
@@ -6,39 +6,30 @@
 
 // Tooltip Init
 $(function() {
-    $("[data-toggle='tooltip']").tooltip();
+  $("[data-toggle='tooltip']").tooltip();
 });
-
-
-// make all images responsive
-/*
- * Unuse by Hux
- * actually only Portfolio-Pages can't use it and only post-img need it.
- * so I modify the _layout/post and CSS to make post-img responsive!
- */
-// $(function() {
-//  $("img").addClass("img-responsive");
-// });
 
 // responsive tables
 $(document).ready(function() {
-    $("table").wrap("<div class='table-responsive'></div>");
-    $("table").addClass("table");
+  $("table").wrap("<div class='table-responsive'></div>");
+  $("table").addClass("table");
 });
 
 // responsive embed videos
-$(document).ready(function () {
-    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
-    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+$(document).ready(function() {
+  $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+  $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
 // 判断是不是博文页面
-function isPages(attr){
-    var currentBoolean = document.querySelector('.navbar.navbar-custom').getAttribute(attr);
-    if(currentBoolean === 'true'){return true;}
-    return false;
+function isPages(attr) {
+  var currentBoolean = document.querySelector('.navbar.navbar-custom').getAttribute(attr);
+  if (currentBoolean === 'true') {
+    return true;
+  }
+  return false;
 }
 /*
     滚动函数
@@ -47,23 +38,20 @@ function isPages(attr){
         2 给目标对象切换class
         3 触发的高度 (可选项,如果不指定高度,会将DOM的高度作为触发高度)
 */
-function scrollCheck(scrollTarget, toggleClass, scrollHeight){
-    document.addEventListener('scroll',function(){
-    var currentTop = window.pageYOffset;
-        currentTop > (scrollHeight||scrollTarget.clientHeight)
-        ?scrollTarget.classList.add(toggleClass)
-        :scrollTarget.classList.remove(toggleClass)
-    })
+function greater(a, b) {
+  return a > b;
 }
+function scrollCheck(scrollTarget, toggleClass, scrollHeight, reversed) {
+  document.addEventListener('scroll', function() {
+    var currentTop = window.pageYOffset;
 
-//主页
-(function(){
-    if(!isPages('data-ispost')){
-        var navbar = document.querySelector('.navbar.navbar-custom')
-        navbar.classList.add('is-fixed');
+    var test = greater(currentTop, scrollHeight || scrollTarget.clientHeight);
+    if (reversed) {
+      test = !test;
     }
-
-})();
+    test ? scrollTarget.classList.add(toggleClass) : scrollTarget.classList.remove(toggleClass);
+  })
+}
 
 /*
 * 先获取H1标签
@@ -71,27 +59,27 @@ function scrollCheck(scrollTarget, toggleClass, scrollHeight){
 * 将其内容放到上面居中显示
 * */
 
-/*
-    博文页面
-*/
-(function(){
-    if (isPages('data-ispost')){
-        var navbar = document.querySelector('.navbar-custom');
-        var introHeader = document.querySelector('.intro-header').offsetHeight;
-        var introHeader = introHeader > 497 ? introHeader : 400;
-        var toc = document.querySelector('.toc-wrap');
-        var postTitle = document.querySelector('.post-title-haojen');
-        scrollCheck(toc,'toc-fixed',introHeader-60);
-        scrollCheck(navbar,'is-fixed');
-        scrollCheck(postTitle,'post-title-fixed',introHeader-60);
-    }
+// Change Blog Post Navbar
+(function() {
+  if (isPages('data-ispost')) {
+    // navbar
+    var navbar = document.querySelector('.navbar-custom');
+    var navbarHeight = 60;
+    var introHeader = document.querySelector('.intro-header').offsetHeight;
+    var introHeader = introHeader > 497 ? introHeader : 400;
+    var postTitle = document.querySelector('.title-calpa');
+
+    // table of content
+    var toc = document.querySelector('.toc-wrap');
+    scrollCheck(toc, 'toc-fixed', introHeader);
+    scrollCheck(navbar, 'is-fixed', navbarHeight);
+    scrollCheck(postTitle, 'display-none', navbarHeight, true);
+  }
 })();
 
-(function () {
-    var navTop = document.querySelector('#nav-top');
-    navTop.ondblclick = function () {
-        $('body').animate({
-            scrollTop: 0
-        }, 500)
-    }
-})();
+function scrolltoTop(event) {
+  event.preventDefault();
+  $('body').animate({
+    scrollTop: 0
+  }, 500);
+}
