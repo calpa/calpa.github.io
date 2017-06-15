e1c663ab872325adeb68c427a761b46a8cdc9ce3
Calpa Liu <calpaliu@gmail.com>
2017-06-15 10:16:24 +0800

    Site updated: 2017-06-15 10:16:24

--- js/magic.js
+++ js/magic.js
@@ -16,3 +16,24 @@ window.onfocus = () => {
     // Restore the original title
     document.title = title.focus;
 };
+
+// fancybox 3.0
+$('.post-container').each(function(i){
+  $(this).find('img').each(function(){
+    if ($(this).parent().hasClass('fancybox')) return;
+
+    var alt = this.alt;
+
+    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+    $(this).wrap('<a data-fancybox="images" href="'+ this.src + '" title="' + alt + '" class="fancybox"></a>');
+  });
+
+  $(this).find('.fancybox').each(function(){
+    $(this).attr('rel', 'article' + i);
+  });
+});
+
+if ($.fancybox){
+  $('[data-fancybox]').fancybox();
+}
